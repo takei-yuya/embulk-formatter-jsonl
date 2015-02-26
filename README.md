9aa03518ee87aac9720c38dc7dffdaa5eece327d
TAKEI Yuya <takei.yuya+github@gmail.com>
2015-02-27 02:10:48 +0900

    encoding and newline options
    rfc4627 allow these encodings

--- README.md
+++ README.md
@@ -10,6 +10,14 @@ Format datum to jsonl, 1 json per 1 line
 
 ## Configuration
 
+- **encoding**: output encoding. must be one of "UTF-8", "UTF-16LE", "UTF-32BE", "UTF-32LE" or "UTF-32BE" (string default: 'UTF-8')
+- **newline**: newline character.
+    - CRLF: use `\r`(0x0d) and `\n`(0x0a) as newline character
+    - LF: use `\n`(0x0a) as newline character
+    - CR: use `\r`(0x0d) as newline character
+    - NUL: use `\0`(0x00) instead of newline (for example, `xargs -0` will be good friend with it)
+    - NO: dump JSONs in a line
+
 ## Example
 
 ```yaml
@@ -17,6 +25,8 @@ out:
   type: any output input plugin type
   formatter:
     type: jsonl
+    encoding: UTF-8
+    newline: NUL
 ```
 
 ## Build
--- lib/embulk/formatter/jsonl.rb
+++ lib/embulk/formatter/jsonl.rb
@@ -6,16 +6,42 @@ module Embulk
     class JsonlFormatterPlugin < FormatterPlugin
       Plugin.register_formatter("jsonl", self)
 
+      VALID_ENCODINGS = %w(UTF-8 UTF-16LE UTF-32BE UTF-32LE UTF-32BE)
+      NEWLINES = {
+        'CRLF' => "\r\n",
+        'LF' => "\n",
+        'CR' => "\r",
+        # following are not jsonl, but useful in some case
+        'NUL' => "\0",
+        'NO' => '',
+      }
+
+      def self.join_texts((*inits,last), opt = {})
+        delim = opt[:delimiter] || ', '
+        last_delim = opt[:last_delimiter] || ' or '
+        [inits.join(delim),last].join(last_delim)
+      end
+
       def self.transaction(config, schema, &control)
         # configuration code:
         task = {
+          'encoding' => config.param('encoding', :string, default: 'UTF-8'),
+          'newline' => config.param('newline', :string, default: 'LF'),
         }
+
+        encoding = task['encoding'].upcase
+        raise "encoding must be one of #{join_texts(VALID_ENCODINGS)}" unless VALID_ENCODINGS.include?(encoding)
+
+        newline = task['newline'].upcase
+        raise "newline must be one of #{join_texts(NEWLINES.keys)}" unless NEWLINES.has_key?(newline)
 
         yield(task)
       end
 
       def init
         # initialization code:
+        @encoding = task['encoding'].upcase
+        @newline = NEWLINES[task['newline'].upcase]
 
         # your data
         @current_file == nil
@@ -36,7 +62,7 @@ module Embulk
           @schema.each do |col|
             datum[col.name] = record[col.index]
           end
-          @current_file.write "#{datum.to_json}\n"
+          @current_file.write "#{datum.to_json}#{@newline}".encode(@encoding)
         end
       end
 
